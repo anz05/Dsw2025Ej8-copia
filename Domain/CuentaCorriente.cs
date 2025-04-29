52983bfc540acc156ae1add799b7f4554001d32f
jomatias108 <jomatias108>
2025-04-29 17:03:05 -0300

    Creacion de clases hijas de cuenta bancaria

---
+++ Domain/CuentaCorriente.cs
@@ -0,0 +1,36 @@
+﻿}using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain;
+
+public class CuentaCorriente : CuentaBancaria
+{
+
+    private decimal _limiteDeDescubierto;
+    public CuentaCorriente(string numero, decimal saldo, string[] titulares) : base(numero, saldo, titulares)
+    {
+    }
+
+    public void Depositar(decimal monto)
+    {
+        monto -= monto * _comision;
+        _saldo += monto;
+    }
+
+    public void Retirar(decimal monto)
+    {
+       if (_saldo - monto >= -_limiteDeDescubierto)
+       {
+          _saldo -= monto;
+       }
+       if (_saldo < 0)
+       {
+           _estado = Estado.Suspendida;
+           
+       }
+    }
+
+}
